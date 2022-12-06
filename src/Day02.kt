e7d4f9d18ed6edddca4c64746f4e74ca4032c2d5
Yurii Sunhurov <yurii.sunhurov@grupawp.pl>
2022-12-07 00:17:13 +0100

    [aoc-2022][day-2] Add second part of day 2 puzzle

--- src/Day02.kt
+++ src/Day02.kt
@@ -4,8 +4,10 @@ fun main() {
     val winCombination = setOf("A Y", "B Z", "C X")
     val drawCombination = setOf("B X", "C Y", "A Z")
 
-    fun part1(input: List<String>): Int =
-        input.sumOf { move ->
+    fun sumScore(moves: List<String>, isDefaultStrategy: Boolean) =
+        moves.map { move ->
+            move.takeIf { isDefaultStrategy } ?: move.swapToNewStrategy()
+        }.sumOf { move ->
             when (move) {
                 in winCombination -> 6
                 in drawCombination -> 0
@@ -13,16 +15,18 @@ fun main() {
             }.plus(move.takeLast(1).asMoveBonus())
         }
 
-    fun part2(input: List<String>): Int = input.size
+    fun part1(input: List<String>): Int = sumScore(input, isDefaultStrategy = true)
+
+    fun part2(input: List<String>): Int = sumScore(input, isDefaultStrategy = false)
 
     val testInput = readInput(name = "${day}_test")
     val input = readInput(name = day)
 
     check(part1(testInput) == 15)
-//    check(part2(testInput) == 12)
+    check(part2(testInput) == 12)
 
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
 
 private fun String.asMoveBonus() =
@@ -30,4 +34,34 @@ private fun String.asMoveBonus() =
         "X" -> 1
         "Y" -> 2
         else -> 3
+    }
+
+private fun String.swapToNewStrategy() =
+    take(2).plus(
+        when (takeLast(1)) {
+            "X" -> first().asLoseMove()
+            "Y" -> first().asDrawMove()
+            else -> first().asWinMove()
+        }
+    )
+
+private fun Char.asLoseMove() =
+    when (this) {
+        'A' -> "Z"
+        'B' -> "X"
+        else -> "Y"
+    }
+
+private fun Char.asDrawMove() =
+    when (this) {
+        'A' -> "X"
+        'B' -> "Y"
+        else -> "Z"
+    }
+
+private fun Char.asWinMove() =
+    when (this) {
+        'A' -> "Y"
+        'B' -> "Z"
+        else -> "X"
     }
