0ab74ab7a2b33e2aeb48b6f57f7f5a4d6182145a
Yurii Sunhurov <yurii.sunhurov@grupawp.pl>
2022-12-10 07:34:55 +0100

    [aoc-2022][day-8] Update 8 day second part puzzle solution

--- src/Day08.kt
+++ src/Day08.kt
@@ -14,41 +14,52 @@ fun main() {
             }
         }
 
+    fun onEachTree(grid: Grid, action: (TreeView) -> Unit) {
+        grid.rows.forEach { (rowIndex, row) ->
+            grid.columns.forEach { (columnIndex, column) ->
+                action(
+                    TreeView(
+                        currentTree = row[columnIndex],
+                        up = column.take(rowIndex).reversed(),
+                        down = column.takeLast(column.lastIndex - rowIndex),
+                        left = row.take(columnIndex).reversed(),
+                        fight = row.takeLast(row.lastIndex - columnIndex)
+                    )
+                )
+            }
+        }
+    }
+
     fun part1(lines: List<String>): Int =
-        getTreeGrid(lines).let { (rows, columns) ->
+        getTreeGrid(lines).let { grid ->
             mutableListOf<Int>().apply {
-                rows.forEach { (rowIndex, row) ->
-                    columns.forEach { (columnIndex, column) ->
-                        val rowBeforeTree = row.take(columnIndex)
-                        val rowAfterTree = row.takeLast(row.lastIndex - columnIndex)
-                        val columnBeforeTree = column.take(rowIndex)
-                        val columnAfterTree = column.takeLast(column.lastIndex - rowIndex)
-                        val currentTree = row[columnIndex]
-                        if (rowBeforeTree.firstOrNull { it >= currentTree } == null
-                            || rowAfterTree.firstOrNull { it >= currentTree } == null
-                            || columnBeforeTree.firstOrNull { it >= currentTree } == null
-                            || columnAfterTree.firstOrNull { it >= currentTree } == null) add(1)
-                    }
+                onEachTree(grid) { (current, up, down, left, right) ->
+                    if (left.firstOrNull { it >= current } == null
+                        || right.firstOrNull { it >= current } == null
+                        || up.firstOrNull { it >= current } == null
+                        || down.firstOrNull { it >= current } == null) add(1)
                 }
             }.sum()
         }
 
+    fun List<Int>.takeUntil(predicate: (Int) -> Boolean): List<Int> {
+        val list = ArrayList<Int>()
+        for (item in this) {
+            list.add(item)
+            if (predicate(item))
+                break
+        }
+        return list
+    }
 
-    fun part2(lines: List<String>): Int = getTreeGrid(lines).let { (rows, columns) ->
+    fun part2(lines: List<String>): Int = getTreeGrid(lines).let { grid ->
         mutableListOf<Int>().apply {
-            rows.forEach { (rowIndex, row) ->
-                columns.forEach { (columnIndex, column) ->
-                    val rowBeforeTree = row.take(columnIndex)
-                    val rowAfterTree = row.takeLast(row.lastIndex - columnIndex)
-                    val columnBeforeTree = column.take(rowIndex)
-                    val columnAfterTree = column.takeLast(column.lastIndex - rowIndex)
-                    val currentTree = row[columnIndex]
-                    val left = rowBeforeTree.takeLastWhile { it <= currentTree }.size
-                    val right = rowAfterTree.takeWhile { it <= currentTree }.size
-                    val up = columnBeforeTree.takeLastWhile { it <= currentTree }.size
-                    val bottom = columnAfterTree.takeWhile { it <= currentTree }.size
-                    add(left * right * up * bottom)
-                }
+            onEachTree(grid) { (current, up, down, left, right) ->
+                val l = left.takeUntil { it >= current }.count()
+                val r = right.takeUntil { it >= current }.count()
+                val u = up.takeUntil { it >= current }.count()
+                val d = down.takeUntil { it >= current }.count()
+                add(l * r * u * d)
             }
         }.max()
     }
@@ -57,7 +68,7 @@ fun main() {
     val input = readInput(name = day)
 
     check(part1(testInput) == 21)
-    check(part2(testInput) == 12)
+    check(part2(testInput) == 8)
 
     println(part1(input))
     println(part2(input))
@@ -66,4 +77,12 @@ fun main() {
 data class Grid(
     val rows: HashMap<Int, List<Int>> = hashMapOf(),
     val columns: HashMap<Int, MutableList<Int>> = hashMapOf()
+)
+
+data class TreeView(
+    val currentTree: Int,
+    val up: List<Int>,
+    val down: List<Int>,
+    val left: List<Int>,
+    val fight: List<Int>,
 )
